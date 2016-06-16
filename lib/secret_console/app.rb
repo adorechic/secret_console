f0b284a38a221d19261fe35c4b99631f71f52ea4
Takamichi Yoshikawa <takamichi-yoshikawa@cookpad.com>
2016-06-16 18:18:52 +0900

    Show secrets

--- lib/secret_console/app.rb
+++ lib/secret_console/app.rb
@@ -1,12 +1,40 @@
 require "sinatra"
+require "rcredstash"
 
 module SecretConsole
   class App < Sinatra::Base
+    DELIMITER = '.'
 
     set :views, File.expand_path('../views', __FILE__)
 
     get '/*' do
-      erb :index
+      path = params['splat'].first.gsub("/", DELIMITER)
+
+      keys = CredStash.list.map { |k, v| k }
+
+      unless path == ""
+        keys = keys.grep(/\A#{path}#{DELIMITER}*/)
+      end
+
+      values = {}
+      links = {}
+
+      keys.each do |key|
+        child_path = key.gsub(path, '')
+        child_path = child_path[1..-1] if child_path.start_with?(DELIMITER)
+        childs = child_path.split(DELIMITER)
+
+        case childs.size
+        when 0
+          # TODO
+        when 1
+          values[childs.first] = CredStash.get(key)
+        else
+          links[childs.first] = [path, '/', childs.first].join
+        end
+      end
+
+      erb :index, locals: { values: values, links: links }
     end
   end
 end
--- secret_console.gemspec
+++ secret_console.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
 
   spec.add_dependency "sinatra"
+  spec.add_dependency "rcredstash", "~> 0.2.0"
 
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
